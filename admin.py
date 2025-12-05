7bf61e7fd66c16e2b81b9e7fe083f48dd6a59aca
Python_Coder <kondiytmc@gmail.com>
2025-12-05 13:57:55 +0500

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Faculty, Student, Subject, Grade
-
-
-admin.site.register(Faculty)
-admin.site.register(Student)
-admin.site.register(Subject)
-admin.site.register(Grade)
